cf4e886b35fe5c118cc1ad80b45a72f1f6018def
g1gasad <asadahsan2245@gmail.com>
2024-09-16 12:26:15 +0530

    a func connot be defined inside another func in C

--- Chapter 2 - Practice Set/01_problem1.c
+++ Chapter 2 - Practice Set/01_problem1.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-int main(){
-    int func_a(){
-    int a = 1; int b = a+3;
-    printf("the value of a is %d\nthe value of b is %d\n", a, b);
+int func_a() {
+int a = 1;
+int b = a+3;
+printf("the value of a is %d\nthe value of b is %d\n", a, b);
+return 0;
+}
+
+int func_b() {
+    char dt = 'd';
+    printf("the string is %c", dt);
     return 0;
-    };
-    char func_b(){
-        char dt = 'd';
-        printf("the string is %c", dt);
-        return 0;
-    }
+}
+
+int main(){
+    func_a();
     func_b();
-    // func_a();
 }
