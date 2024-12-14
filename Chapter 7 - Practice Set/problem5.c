d6b9f032f248f9c9b11ec7e7588e8ca271e46f8e
g1gasad <asadahsan2245@gmail.com>
2024-12-14 19:21:39 +0530

    reversed an array

--- Chapter 7 - Practice Set/problem5.c
+++ Chapter 7 - Practice Set/problem5.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-
-void reverse(int *ptr, int size);
-void reverse(int *ptr, int size)
+int arrDisplay(int arr[], int size)
 {
+    printf("the array is:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+void reverse(int arr[], int size)
+{
+    int temp;
+    for (int i = 0; i < size / 2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
+    }
 }
 
 int main()
 {
-    int arr[6] = {4, 6, 1, 9, 7, 0};
+    int arr[11] = {4, 6, 111, 9, 7, -5, 23, 69, 0, -77, 619};
     int *ptr = arr;
-    printf("the array is:\n");
-    for (int i = 0; i < 6; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-
-    // reverse(ptr, 5);
+    arrDisplay(arr, 11);
+    reverse(arr, 11);
+    arrDisplay(arr, 11);
     return 0;
 }
