9bd2d9ae0ec4c1c60781b7ba892db558157136d9
g1gasad <asadahsan2245@gmail.com>
2025-01-10 18:45:13 +0530

    finally pushed code after 10 days

---
+++ Chapter 8 - Practice Set/problem2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str1[10], str2[10];
+    printf("kindly input string 1: ");
+    scanf("%c", &str1);
+    printf("kindly input string 2: ");
+    scanf("%s", &str2);
+
+    if('a'=='a'){
+        printf("same");
+    }
+
+    return 0;
+}
--- Chapter 8 - Strings/4_gets_puts.c
+++ Chapter 8 - Strings/4_gets_puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    char str[50];
+    char str1[50], str2[50];
 // used to input multiword strings
 
-    gets(str);
+    gets(str1);
+    gets(str2);
     // printf("%s", str);
-    puts(str);
-    printf("Wow");
-
+    puts(str1);
+    puts(str2);
+    // printf("Wow");
+    
     return 0;
 }
