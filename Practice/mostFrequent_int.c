7ad5f15a6ac849029d4f54debd6b2c3ae8a98fd7
Asad <asadahsan2245@gmail.com>
2025-07-02 08:56:29 +0530

    most freq int and frequency

--- Practice/mostFrequent_int.c
+++ Practice/mostFrequent_int.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-void display(int arr[]){
-    for(int i=0; i<50; i++){
-        printf("%d ", arr[i]);
+void displayMax(int arr[]){
+    int max=arr[0]; 
+    int value = 0;
+    for(int i=0; i<100; i++){
+        if(arr[i]>max){
+            max=arr[i];
+            value=i;
+        }
     }
+    printf("Mode of the data is %d\nMax frequency is %d\n", value, max);
 }
 int main(){
     int arr[] = {3, 6, 1, 1, 1, 5, 7, 90, 69, 0, 3, 6, 1, 1, 5};
-    int uniq[50];
-    int freq[50] = {0};
-    int val;
+    int freq[100] = {0};
     for(int i=0; i<sizeof(arr)/4; i++){
-        val = arr[i];
-        for (int j=0; j<51; j++){
-            if (val==uniq[j]){
-                freq[i]++;
-                break;
-            }
-        }
-        freq[i]++;
-        uniq[i] = val;
+        freq[arr[i]]++;
     }
-    display(uniq);
+    displayMax(freq);
     return 0;
 }
---
+++ Practice/remove_all_vowels_from_str.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[] = "Orphan Village";
+    for(int i=0; i<strlen(str); i++){
+        if((str[i]!='a') && (str[i]!='e') && (str[i]!='i') && (str[i]!='o') && (str[i]!='u')
+        && (str[i]!='A') && (str[i]!='E') && (str[i]!='I') && (str[i]!='O') &&  (str[i]!='U')){
+            printf("%c", str[i]);
+        }
+    }
+    return 0;
+}
